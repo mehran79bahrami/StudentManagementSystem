631a85aa40316fec34b07a4e3aeaab6e977488c5
Mehran <Mehran@DESKTOP-ULD5K9G>
2026-08-08 01:38:28 +0330

    Add project files.

---
+++ StudentManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
