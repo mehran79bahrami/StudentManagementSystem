0fc6ffaa62326152a94312e3c222938287651b76
Mehran <207275219+mehran79bahrami@users.noreply.github.com>
2026-08-08 20:31:30 +0330

    Complete student data model class

--- StudentManagementSystem/Student.cs
+++ StudentManagementSystem/Student.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace StudentManagementSystem
 {
-    internal class Student
+    public class Student
     {
+
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public Student(int id, string fullname, int age, string email, string phonenumber)
+        {
+            this.ID = id;
+            this.FullName = fullname;
+            this.Age = age;
+            this.Email = email;
+            this.PhoneNumber = phonenumber;
+            this.RegisterDate = DateTime.Now;
+        }
+
     }
 }
