c852536f9e5a5971da59d708b9fcba12fd82191f
Mehran <207275219+mehran79bahrami@users.noreply.github.com>
2026-08-09 22:38:46 +0330

    Add AddStudent, RemoveStudent , ShowStudentList ,SearchStudentFullName to StudentManager

--- StudentManagementSystem/StudentManager.cs
+++ StudentManagementSystem/StudentManager.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace StudentManagementSystem
 {
-    internal class StudentManager
+    public class StudentManager
     {
+        private List<Student> StudentList = new List<Student>();
+        private int IdCounter = 1000;
+
+
+        public void AddStudent(string fullname, int age, string email, string phonenumber)
+        {
+            Student s = new Student(IdCounter++, fullname, age, email, phonenumber);
+            StudentList.Add(s);
+        }
+
+
+        public bool RemoveStudent(int id)
+        {
+            foreach (Student temp in StudentList)
+            {
+                if (temp.ID == id)
+                {
+                    StudentList.Remove(temp);
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public List<Student> ShowStudentList()
+        {
+            return StudentList;
+        }
+
+        public List<Student> SearchStudentFullName(string fullname)
+        {
+            List<Student> ResultList = new List<Student>();
+            foreach (Student temp in StudentList)
+            {
+                if (temp.FullName.ToLower() == fullname.ToLower())
+                {
+                    ResultList.Add(temp);
+                }
+            }
+            return ResultList;
+        }
+
+        //todo
     }
 }
