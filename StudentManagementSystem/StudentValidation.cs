5df710e3207531537e1e113f8686777559c710fc
Mehran <207275219+mehran79bahrami@users.noreply.github.com>
2026-08-10 15:48:52 +0330

    feat(StudentValidation) : add fullname validation rules and complete ValidateFullName

--- StudentManagementSystem/StudentValidation.cs
+++ StudentManagementSystem/StudentValidation.cs
@@ -6,9 +6,38 @@ namespace StudentManagementSystem
 {
     public static class StudentValidation
     {
+
         public static string ValidateFullName(string fullname)
         {
-            //todo
+
+            if (fullname.Any(char.IsDigit))
+            {
+                return "Full name cannot contains number";
+            }
+
+            else if (string.IsNullOrWhiteSpace(fullname))
+            {
+                return "Full name cannot be empty";
+            }
+
+            else if (!fullname.Contains(" "))
+            {
+                return "Enter both first and last name separated by a space";
+            }
+            else if (fullname[0] == ' ' || fullname[fullname.Length - 1] == ' ')
+            {
+                return "Name cannot start or end with space";
+
+            }
+            else if (fullname.Length > 30)
+            {
+                return "Name is too long (MAX 30 characters)";
+            }
+            else if (fullname.Length < 5)
+            {
+                return "Name is too short (MIN 5 characters)";
+            }
+
             return null;
         }
 
