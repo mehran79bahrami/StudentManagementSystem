81aed37464f70167dedfe0de2f564acb011167fb
Mehran <207275219+mehran79bahrami@users.noreply.github.com>
2026-08-11 14:55:05 +0330

    feat(Program): complete AddStudentFlow method

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -1,8 +1,12 @@
-﻿namespace StudentManagementSystem
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StudentManagementSystem
 {
     internal class Program
     {
         static StudentManager manager = new StudentManager();
+
+
 
         static void ShowMenu()
         {
@@ -24,10 +28,11 @@
 
         }
 
-        
+
 
         static void Main(string[] args)
         {
+
 
             while (true)
             {
@@ -37,8 +42,7 @@
                 switch (choise)
                 {
                     case "1":
-                        Console.WriteLine("Add Student");
-                        //todo
+                        AddStudentFlow();
                         break;
                     case "2":
                         Console.WriteLine("Remove Student");
@@ -61,11 +65,11 @@
                         //todo
                         break;
                     case "7":
-                        SortbyName();
+                        SortbyNameFlow();
 
                         break;
                     case "8":
-                        SortbyAge();
+                        SortbyAgeFlow();
                         break;
                     case "9":
                         ShowTotalCount();
@@ -76,7 +80,8 @@
                         Console.WriteLine("----- GoodBye -----");
                         Environment.Exit(0);
                         break;
-                    default : Console.Clear();
+                    default:
+                        Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Invalid choice. Please try again");
                         Console.ResetColor();
@@ -99,7 +104,7 @@
             }
         }
 
-        static void SortbyName()
+        static void SortbyNameFlow()
         {
             Console.Clear();
             Console.WriteLine("----- Sort by Name -----");
@@ -114,7 +119,7 @@
         }
 
 
-        static void SortbyAge()
+        static void SortbyAgeFlow()
         {
             Console.Clear();
             Console.WriteLine("----- Sort by Age -----");
@@ -134,10 +139,110 @@
             Console.WriteLine("----- Show Total Count -----");
             int total = manager.StudentCount();
             Console.WriteLine($"Total students in the system: {total}");
-            
-            
 
 
+
+
+        }
+
+
+
+
+        static void AddStudentFlow()
+        {
+            Console.Clear();
+            Console.WriteLine("----- Add Student -----");
+            Console.WriteLine("you can type 'cancel' to abort");
+            string FullName = null;
+            int age = 0;
+            string Email = null;
+            string PhoneNumber = null;
+
+            while (true)
+            {
+                Console.Write("\nEnter Full Name: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "cancel")
+                {
+                    return;
+                }
+                string error = StudentValidation.ValidateFullName(input);
+                if (error == null)
+                {
+                    FullName= input;
+                    break;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ResetColor();
+
+            }
+
+            while (true)
+            {
+                Console.Write("\nEnter Age: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "cancel")
+                {
+                    return;
+                }
+                string error = StudentValidation.ValidateAge(input, out int NewAge);
+                if (error == null && NewAge!=0)
+                {
+                    age= NewAge;
+                    break;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ResetColor();
+            }
+
+            while (true)
+            {
+                Console.Write("\nEnter Phone Number: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "cancel")
+                {
+                    return;
+                }
+                string error = StudentValidation.ValidatePhoneNumber(input);
+                if (error == null)
+                {
+                    PhoneNumber = input;
+                    break;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ResetColor();
+            }
+
+            while (true)
+            {
+                Console.Write("\nEnter Email: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "cancel")
+                {
+                    return;
+                }
+                string error = StudentValidation.ValidateEmail(input);
+                if (error == null)
+                {
+                    Email = input;
+                    break;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(error);
+                Console.ResetColor();
+            }
+
+            manager.AddStudent(FullName, age,Email,PhoneNumber);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("* * * * Student added successfully * * * *");
+            Console.ResetColor();
         }
     }
 }
